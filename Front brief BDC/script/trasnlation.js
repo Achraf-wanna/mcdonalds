ee713650b86d61421b7e8139a3519f8ba91cfc95
DaifAB <abd.daif96@gmail.com>
2021-02-11 15:31:43 +0100

    add database files

--- Front brief BDC/script/trasnlation.js
+++ Front brief BDC/script/trasnlation.js
@@ -12,9 +12,19 @@ arrLang = {
         "dining" : "DINING LOCATION",
         "in" : "Take in",
         "out" : "Take out",
-        "payement" : "WHO WOULD YOU LIKE TO PAY ?",
+        "payement" : "HOW WOULD YOU LIKE TO PAY ?",
         "cc" : "CREDIT CARD",
-        "cash" : "CASH AT COUNTER"
+        "cash" : "CASH AT COUNTER",
+        "extra" : "Extra :",
+        "addExtra" : "Add Extra",
+        "prodQuantity" : "Product Quantity :",
+        "tableServ" : "Table service number :",
+        "promoCode" : "Promo code :",
+        "check" : "Check",
+        "fideleCode" : "Fidelity card code :",
+        "congrats" : "Congratulations !",
+        "effectuer" : "Your order has been successfully completed !",
+        "close" : "Close"
     },
     fr : {
         "categories":"CATEGORIE",
@@ -24,8 +34,41 @@ arrLang = {
         "out" : "Emporté",
         "payement" : "COMMENT VOUDEREZ-VOUS PAYER ?",
         "cc" : "CARTE CREDIT",
-        "cash" : "PAYER EN ESPÈCES"
+        "cash" : "PAYER EN ESPÈCES",
+        "extra" : "Supplémentaire :",
+        "addExtra" : "Ajouter supplémentaire",
+        "prodQuantity" : "Quantité de produit :",
+        "tableServ" : "Numéro de service à table :",
+        "promoCode" : "Code promo :",
+        "check" : "Vérifier",
+        "fideleCode" : "Code carte fidélité :",
+        "congrats" : "Félicitations !",
+        "effectuer" : "Votre commande a été bien effectuer !",
+        "close" : "Fermer"
+    },
+
+    ar : {
+        "categories":"الفئة",
+        "back":"عودة",
+        "dining" : "مكان تناول الطعام",
+        "in" : "في المنطقة",
+        "out" : "حمَل ",
+        "payement" : "كيف تريد الدفع ؟",
+        "cc" : "بطاقة إئتمان",
+        "cash" : "الدفع نقدا",
+        "extra" : "إضافي :",
+        "addExtra" : "أضف إضافي",
+        "prodQuantity" : "كمية المنتج :",
+        "tableServ" : "رقم خدمة الطاولة :",
+        "promoCode" : "رمز ترويجي",
+        "check" : "تحقق",
+        "fideleCode" : "رمز بطاقة الإخلاص :",
+        "congrats" : "تهانينا !",
+        "effectuer" : "تم إكمال طلبك بنجاح! !",
+        "close" : "اغلاق"
     }
+  
+
 }
 
 function translate() {
@@ -36,3 +79,4 @@ function translate() {
         elements.item(index).innerHTML = arrLang[lang][elements.item(index).getAttribute('key')];
     }
 }
+
